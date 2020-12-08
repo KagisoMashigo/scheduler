58dcdf399692115547959a4696ceb394b3cb92d6
Masterkakes <kagiso.k.mashigo@gmail.com>
2020-12-08 14:35:34 -0500

    remove unecessary coe

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,27 @@
+import React from "react";
+import InterviewerListItem from "components/InterviewerListItem.js";
+import "components/InterviewerList.scss";
+
+
+export default function InterviewerList(props) {
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">    
+      {
+        props.interviewers.map((interviewer) => {
+         return <InterviewerListItem
+            //must be called key for any unique property or react will thr error
+            key={interviewer.id}
+            name={interviewer.name}
+            avatar={interviewer.avatar}
+            selected={interviewer.id === props.interviewer}
+            setInterviewer={props.setInterviewer}
+            />
+        })
+      } 
+      </ul>
+    </section>
+  );
+}
