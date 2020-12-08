99bd6585a6b622f52eaa0cf5451f93af95906a06
Masterkakes <kagiso.k.mashigo@gmail.com>
2020-12-08 15:46:53 -0500

    change setDay to use day id

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -13,7 +13,7 @@ export default function DayList(props) {
             name={day.name} 
             spots={day.spots} 
             selected={day.name === props.day}
-            setDay={props.setDay}  
+            setDay={(event) => props.setDay(day.id)}  
             />
         })
       }
