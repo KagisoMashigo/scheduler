9ef928d96b46bb4baa50660cac83f021c88b1d23
Masterkakes <kagiso.k.mashigo@gmail.com>
2020-12-08 08:55:08 -0500

    complete dayListItem logic

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -16,6 +16,7 @@ const formatSpots = (props) => {
 
 export default function DayListItem(props) {
 
+  
   const dayClass = classNames("day-list__item", {
     "day-list__item": true, 
     "day-list__item--selected": props.selected,
