8139ef2e3090201faf0f4fb46b3319fb6f0cc3a8
Masterkakes <kagiso.k.mashigo@gmail.com>
2020-12-08 20:13:48 -0500

    Status and Confirm components added to story

--- stories/index.js
+++ stories/index.js
@@ -14,8 +14,8 @@ import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header.js";
 import Empty from "components/Appointment/Empty.js";
 import Show from "components/Appointment/Show.js";
-
-
+import Confirm from "components/Appointment/Confirm.js";
+import Status from "components/Appointment/Status.js";
 
 
 
@@ -151,6 +151,18 @@ storiesOf("Appointment", module)
       interviewer={interviewers[0]}
       onEdit={action("onEdit")}
       onDelete={action("onDelete")}
+      />)
+  .add("Confirm", () => 
+      <Confirm  
+      message="Delete the appointment?"
+      onConfirm={action("onConfirm")}
+      onCancel={action("onCancel")}
+      />)
+  .add("Status", () => 
+      <Status  
+      message="Delete the appointment?"
+      onConfirm={action("onConfirm")}
+      onCancel={action("onCancel")}
       />)
 
 
