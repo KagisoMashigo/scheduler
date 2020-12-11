9c595646af9c57cfc0647696c37e20033499fc10
Masterkakes <kagiso.k.mashigo@gmail.com>
2020-12-10 19:48:24 -0500

    add interview functionality

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "components/Appointment/styles.scss";
-
+import Header from "components/Appointment/Header.js";
+import Empty from "components/Appointment/Empty.js";
+import Show from "components/Appointment/Show.js";
 
 export default function Appointment(props) {
-
   return (
-    <article className="appointment"></article>
+    
+    <article className="appointment">
+     <Header time={props.time} />
+     {( 
+       props.interview ? 
+       <Show student={props.interview.student} interviewer={props.interview.interviewer}/>
+       :<Empty />
+     )}
+    </article>
+
   );
 
 }
