f6e682556c04a687a74b1d5ed29b233056e42bce
Masterkakes <kagiso.k.mashigo@gmail.com>
2020-12-11 14:55:32 -0500

    getIV fct added

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,9 +1,32 @@
+// review this function
 export function getAppointmentsForDay(state, day) {
-  const validDays = state.days.map(day => day.name);
-  
-  if (!day || !validDays.includes(day)) return [];
 
-  return state.days
-    .filter(appointment => appointment.name === day)[0]
-    .appointments.map(apptId => state.appointments[apptId]);
+  if(!state.days){
+    return [];
+  }
+  let theDay = state.days.filter(d => d.name === day)[0];
+  if(!theDay){
+    return [];
+  }
+  let result = [];
+  for(const id of theDay.appointments){
+    const appointmentObj = state.appointments[id];
+    result.push(appointmentObj);
+  }
+
+  return result;
+}
+
+export function getInterview(state, interview) {
+  let interviewersObj = state.interviewers;
+  let result = {};
+
+  if(!interviewersObj || !interview){
+    return null;
+  }
+
+  result.interviewer = interviewersObj[interview.interviewer]
+  result.student = interview.student
+
+  return result;
 }
