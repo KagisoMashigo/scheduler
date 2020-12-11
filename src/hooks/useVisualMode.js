47c5cc69cafca73735b8cea0eb6899f144c5c194
Masterkakes <kagiso.k.mashigo@gmail.com>
2020-12-11 18:41:13 -0500

    add custom useVisual hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (newMode, replace = false) => {
+
+    if (!replace) {
+      setMode(newMode);
+      history.push(newMode)
+    } else {
+      setMode(newMode)
+    }
+
+  }
+
+  const back = () => {
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length - 1])
+    }
+  }
+
+  return {
+    mode,
+    transition,
+    back
+  };
+}
