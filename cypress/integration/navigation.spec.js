e319a9a4d76aee5051acf67845b64a8d180faa34
Masterkakes <kagiso.k.mashigo@gmail.com>
2020-12-16 17:48:53 -0500

    navigation test complete

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,4 +1,5 @@
 describe("Navigation", () => {
+
   it("should visit root", () => {
     cy.visit("/");
   });
@@ -6,9 +7,10 @@ describe("Navigation", () => {
   it("should navigate to Tuesday", () => {
     cy.visit("/");
 
-    cy.contains("li", 'Tuesday')
+    cy.contains("[data-testid=day]", "Tuesday")
     .click()
-    .should("have.css", "background-color", "rgb(242, 242, 242)");
-    
+    .should("have.class", "day-list__item--selected")
+
   });
+
 });
