16914c8019b564fdbbc6daf98ad02bb329834436
Masterkakes <kagiso.k.mashigo@gmail.com>
2020-12-17 09:55:05 -0500

    fix selecting interviewer null

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -21,6 +21,9 @@ export default function Form(props) {
     if (name === "") {
       setError("Student name cannot be blank");
       return;
+    } else if (!interviewer) {
+      setError("Please select an interviewer");
+      return;
     }
     setError("");
     props.onSave(name, interviewer);
